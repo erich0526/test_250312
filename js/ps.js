7efe27adb32e055d595feae2e76f4846487d622b
erich0526 <erich0526@gmail.com>
2025-03-13 12:16:45 +0800

    3.6.0

--- js/ps.js
+++ js/ps.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 取得按鈕
+    let Homebutton = document.getElementById('HomePage');
+
+    // 當按鈕被點擊時
+    Homebutton.addEventListener('click', function () {
+        window.open('index.html', '_self');
+    });
+
+});
+
 function checkPasswordStrength() {
     var password = document.getElementById('password').value;
     var strengthBar = document.getElementById('strength-bar');
---
+++ js/wifi.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 取得按鈕
+    let Homebutton = document.getElementById('HomePage');
+
+    // 當按鈕被點擊時
+    Homebutton.addEventListener('click', function () {
+        window.open('index.html', '_self');
+    });
+
+});
