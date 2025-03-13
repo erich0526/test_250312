834b0204b3c802714c2f8f1c7fe5760983ab5bba
erich0526 <erich0526@gmail.com>
2025-03-13 11:28:14 +0800

    0.3.4.5

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // // 創建按鈕
-    // let button = document.createElement("button");
-    // button.textContent = "前往新頁面";
-    // button.style.padding = "10px 20px";
-    // button.style.fontSize = "16px";
-    // button.style.marginTop = "20px";
+    // 創建按鈕
+    let button = document.createElement("button");
+    button.textContent = "前往新頁面";
+    button.style.padding = "10px 20px";
+    button.style.fontSize = "16px";
+    button.style.marginTop = "20px";
     
-    // // 按鈕點擊事件
-    // button.addEventListener("click", function () {
-    //     window.open("password.html", "_self");
-    // });
+    // 按鈕點擊事件
+    button.addEventListener("click", function () {
+        window.open("password.html", "_self");
+    });
 
-    // 找到現有的 Wi-Fi 按鈕
     let wifiButton = document.createElement("goToWiFiPage");
     wifiButton.textContent = "使用公用網路的風險";
     wifiButton.style.padding = "10px 20px";
@@ -25,4 +24,5 @@ document.addEventListener("DOMContentLoaded", function () {
     
     // 將創建的按鈕添加到頁面
     document.body.appendChild(button);
+    document.body.appendChild(wifiButton);
 });
