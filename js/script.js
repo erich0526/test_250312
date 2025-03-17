c775c2ac441bb789c22d9ddbcd713e38883e2048
erich0526 <erich0526@gmail.com>
2025-03-17 23:05:12 +0800

    correction

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // 取得按鈕
     let button = document.getElementById('Password');
     let wifiButton = document.getElementById('WiFiPage');
+    let templete = document.getElementById('templete');
+
 
     // 當按鈕被點擊時
     button.addEventListener('click', function () {
@@ -10,5 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     wifiButton.addEventListener('click', function () {
         window.open('html/wifi.html', '_self');
+    });
+
+    templete.addEventListener('click', function () {
+        window.open('html/templete.html', '_self');
     });
 });
