798d0f772845d964bcc38db2be4172170c1a6fdc
hsiaoeric0526 <hsiaoeric87@gmail.com>
2025-03-24 11:49:57 +0800

    Update wifi.js

--- js/wifi.js
+++ js/wifi.js
@@ -1,12 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     // 取得按鈕
-//     let Homebutton = document.getElementById('HomePage');
-
-//     // 當按鈕被點擊時
-//     Homebutton.addEventListener('click', function () {
-//         window.open('../index.html', '_self');
-//     });
-// });
 document.addEventListener("DOMContentLoaded", function () {
     const content_text = document.getElementById("content");
     const Button_A = document.getElementById("A");
