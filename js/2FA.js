9c58f067bc3932e895f63303bbba5619b9f15d6b
hsiaoeric0526 <hsiaoeric87@gmail.com>
2025-03-24 12:29:32 +0800

    Create 2FA.js

---
+++ js/2FA.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const content_text = document.getElementById("content");
+    const Button_A = document.getElementById("A");
+    const Button_B = document.getElementById("B");
+    const Button_C = document.getElementById("C");
+    const Button_D = document.getElementById("D");
+
+    Button_A.addEventListener("click", function () {
+        content_text.textContent = "你的資料被竊取了!";
+    });
+
+    Button_B.addEventListener("click", function () {
+        content_text.textContent = "非常好!成功保護自己的資料!";
+    });
+
+    Button_C.addEventListener("click", function () {
+        content_text.textContent = "非常好!成功保護自己的資料!";
+    });
+
+    Button_D.addEventListener("click", function () {
+        content_text.textContent = "你朋友的資料被竊取了!你超缺德";
+    });
+});
