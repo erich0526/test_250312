553c4aa7357c0db42d2d6f2242f0e0568d50d7f5
hsiaoeric0526 <hsiaoeric87@gmail.com>
2025-03-24 11:33:51 +0800

    .

--- js/wifi.js
+++ js/wifi.js
@@ -7,4 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
         window.open('../index.html', '_self');
     });
 
+    const content_text = document.getElementById("content");
+    const Button_A = document.getElementById("A");
+    const Button_B = document.getElementById("B");
+    const Button_C = document.getElementById("C");
+    const Button_D = document.getElementById("D");
+
+    Button_A.addEventListener("click", function () {
+        content_text.textContent = "你的資料被竊取了!";
+    });
+
+    Button_B.addEventListener("click", function () {
+        content_text.textContent = "你的資料被竊取了!";
+    });
+
+    Button_C.addEventListener("click", function () {
+        content_text.textContent = "非常好!成功保護自己的資料!";
+    });
+
+    Button_D.addEventListener("click", function () {
+        content_text.textContent = "甚麼事都沒發生，又是個開心的一天";
+    });
 });
