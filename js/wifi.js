889fff101556da21a94da1dd3cd2d6aa8415478c
hsiaoeric0526 <hsiaoeric87@gmail.com>
2025-03-24 11:42:26 +0800

    .

--- js/wifi.js
+++ js/wifi.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // 取得按鈕
-    let Homebutton = document.getElementById('HomePage');
+    // // 取得按鈕
+    // let Homebutton = document.getElementById('HomePage');
 
-    // 當按鈕被點擊時
-    Homebutton.addEventListener('click', function () {
-        window.open('../index.html', '_self');
-    });
+    // // 當按鈕被點擊時
+    // Homebutton.addEventListener('click', function () {
+    //     window.open('../index.html', '_self');
+    // });
 
     const content_text = document.getElementById("content");
     const Button_A = document.getElementById("A");
