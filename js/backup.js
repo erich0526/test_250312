91abed1d1cb520cea1d60e690cee944974a328d5
hsiaoeric0526 <hsiaoeric87@gmail.com>
2025-03-25 17:14:55 +0800

    .

--- js/backup.js
+++ js/backup.js
@@ -6,18 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const Button_D = document.getElementById("D");
 
     Button_A.addEventListener("click", function () {
-        content_text.textContent = "Blastoise used Hydro Pump!";
+        content_text.textContent = "所有重要資料及珍貴照片都完好無缺!";
     });
 
     Button_B.addEventListener("click", function () {
-        content_text.textContent = "Opened the bag...";
+        content_text.textContent = "電腦被貓咪打翻的飲料潑到，整台報廢，甚麼都沒有了...";
     });
 
     Button_C.addEventListener("click", function () {
-        content_text.textContent = "Choose another Pokémon...";
+        content_text.textContent = "半年前出國玩的照片全部不見，要哭了...";
     });
 
     Button_D.addEventListener("click", function () {
-        content_text.textContent = "Can't escape!";
+        content_text.textContent = "明天要交的工作進度被誤刪，要完蛋了...";
     });
 });
--- js/update.js
+++ js/update.js
@@ -6,18 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const Button_D = document.getElementById("D");
 
     Button_A.addEventListener("click", function () {
-        content_text.textContent = "Blastoise used Hydro Pump!";
+        content_text.textContent = "幸運躲過上個版本的重大漏洞..."
     });
-
     Button_B.addEventListener("click", function () {
-        content_text.textContent = "Opened the bag...";
+        content_text.textContent = "個資已洩漏，請盡速更換密碼。";
     });
 
     Button_C.addEventListener("click", function () {
-        content_text.textContent = "Choose another Pokémon...";
+        content_text.textContent = "銀行帳戶已被盜用，請盡速通知銀行。";
     });
 
     Button_D.addEventListener("click", function () {
-        content_text.textContent = "Can't escape!";
+        content_text.textContent = "電腦被植入勒索病毒，資料已被加密。";
     });
 });
