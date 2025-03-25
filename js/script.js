0b85e1531531be93e940f3a75442d0622310699b
hsiaoeric0526 <hsiaoeric87@gmail.com>
2025-03-25 16:50:19 +0800

    .

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // 取得按鈕
     let button = document.getElementById('Password');
     let wifiButton = document.getElementById('WiFiPage');
-    let templete = document.getElementById('templete');
     let FA = document.getElementById('2FA');
     let backup = document.getElementById('backup');
     let update = document.getElementById('update');
+    let status = document.getElementById('status');
 
 
     // 當按鈕被點擊時
@@ -15,10 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     wifiButton.addEventListener('click', function () {
         window.open('html/wifi.html', '_self');
-    });
-
-    templete.addEventListener('click', function () {
-        window.open('html/templete.html', '_self');
     });
 
     FA.addEventListener('click', function () {
@@ -31,5 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     update.addEventListener('click', function () {
         window.open('html/update.html', '_self');
+    });
+
+    status.addEventListener('click', function () {
+        window.open('html/status.html', '_self');
     });
 });
